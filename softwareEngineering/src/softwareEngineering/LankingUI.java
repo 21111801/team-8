84db9da5997ad522d817a77730eaa5e4d6b84632
Kimnaeun <skdms0755@naver.com>
2016-11-26 21:24:15 +0900

    랭킹 이름 입력

--- softwareEngineering/src/softwareEngineering/LankingUI.java
+++ softwareEngineering/src/softwareEngineering/LankingUI.java
@@ -13,10 +13,19 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import java.awt.Font;
 import javax.swing.DropMode;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.SwingConstants;
 
 public class LankingUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +47,7 @@ public class LankingUI extends JFrame {
 	 * Create the frame.
 	 */
 	public LankingUI() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -50,11 +60,36 @@ public class LankingUI extends JFrame {
 		panel.setLayout(null);
 		
 		JTextPane textPane = new JTextPane();
+		textPane.setBackground(SystemColor.menu);
 		textPane.setEditable(false);
-		textPane.setFont(new Font("Koverwatch", Font.PLAIN, 25));
+		textPane.setFont(new Font("Koverwatch", Font.ITALIC, 30));
 		textPane.setText("\uC774\uB984\uC744 \uC785\uB825\uD558\uC138\uC694");
-		textPane.setBounds(12, 26, 192, 31);
+		textPane.setBounds(122, 44, 192, 31);
 		textPane.setAlignmentX(JTextPane.CENTER_ALIGNMENT);
 		panel.add(textPane);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setFont(new Font("«‘√ ∑“µ∏øÚ", Font.PLAIN, 18));
+		textField.setBounds(73, 109, 288, 38);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton button = new JButton("\uD655\uC778");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String nowname;
+				nowname = textField.getText();
+				System.out.println(nowname);
+				
+				
+			}
+		});
+		
+		
+		button.setFont(new Font("Koverwatch", Font.PLAIN, 24));
+		button.setBounds(152, 175, 97, 23);
+		panel.add(button);
 	}
 }
